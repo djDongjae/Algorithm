995922d2d0d7ed9b2fcde36db2eaa8f255987f15
djdongjae <jk47wee@gmail.com>
2024-03-26 09:13:44 +0900

    [PGS] n의 배수 / Lv.0 / 3분

---
+++ dj_dong_jae/src/BOJ_181937/Solution.java
@@ -0,0 +1,11 @@
+package BOJ_181937;
+
+class Solution {
+    public int solution(int num, int n) {
+        if (num % n == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
