628a0e06a4a1c669113e1106b06e6854b8a44643
djdongjae <jk47wee@gmail.com>
2024-04-20 18:34:11 +0900

    [PGS] 문자 리스트를 문자열로 변환하기 / Lv.0 / 5분

---
+++ dj_dong_jae/src/PGS_181941/Solution.java
@@ -0,0 +1,13 @@
+package PGS_181941;
+
+class Solution {
+    public String solution(String[] arr) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String x : arr) {
+            sb.append(x);
+        }
+
+        return sb.toString();
+    }
+}
