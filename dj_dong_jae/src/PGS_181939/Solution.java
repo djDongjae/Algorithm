ac2dc4181b4ac05f8bfbb55b9726512e033d6bf7
djdongjae <jk47wee@gmail.com>
2024-04-22 09:13:44 +0900

    [PGS] 더 크게 합치기 / Lv.0 / 5분

---
+++ dj_dong_jae/src/PGS_181939/Solution.java
@@ -0,0 +1,17 @@
+package PGS_181939;
+
+class Solution {
+    public int solution(int a, int b) {
+        String str1 = Integer.toString(a) + Integer.toString(b);
+        String str2 = Integer.toString(b) + Integer.toString(a);
+
+        int result1 = Integer.parseInt(str1);
+        int result2 = Integer.parseInt(str2);
+
+        if (result1 > result2) {
+            return result1;
+        } else {
+            return result2;
+        }
+    }
+}
