a144703094c6251b9d8489e12289ffa22067a797
djdongjae <jk47wee@gmail.com>
2024-06-22 09:13:44 +0900

    [PGS] 문자열의 뒤의 n글자 / Lv.0 / 1분

---
+++ dj_dong_jae/src/PGS_181910/Solution.java
@@ -0,0 +1,8 @@
+package PGS_181910;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        int start = my_string.length() - n;
+        return my_string.substring(start);
+    }
+}
