3eebe424e893337a79e69e8a878f2ab2335f6a7b
djdongjae <jk47wee@gmail.com>
2024-09-01 20:07:57 +0900

    [BOJ] 11004 K번째 수 / 실버5 / 10분

---
+++ dj_dong_jae/src/BOJ_11004/Main.java
@@ -0,0 +1,28 @@
+package BOJ_11004;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n, k;
+    public static int[] array;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        array = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(array);
+
+        System.out.println(array[k-1]);
+    }
+}
