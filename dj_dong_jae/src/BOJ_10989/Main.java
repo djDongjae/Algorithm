161421768e8547a1180f88e2c9f2d325d2f174e3
djdongjae <jk47wee@gmail.com>
2024-09-04 13:29:35 +0900

    [BOJ] 10989 수 정렬하기 3 / 브론즈1 / 5분

---
+++ dj_dong_jae/src/BOJ_10989/Main.java
@@ -0,0 +1,29 @@
+package BOJ_10989;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n;
+    public static int[] array;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(array);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(array[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
